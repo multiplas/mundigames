0d498ee7d7175db58a19f1df4a8a069e49ef9d46
multiplas <multiplas72@gmail.com>
2017-03-23 00:09:46 +0100

    Añadido splash image when apk is open. Delay 3, we can change the img changing the route, current is the same than launcher img called on mimap. Revise code analysis.

---
+++ app/src/main/java/net/mundigames/mundigames/SplashScreen.java
@@ -0,0 +1,38 @@
+package net.mundigames.mundigames;
+
+/**
+ * Created by javid on 23/3/17.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends MainActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
